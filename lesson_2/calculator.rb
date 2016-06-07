3ffef5de24e3adaa86a4d2ea4b30d148ac1db34e
Elie Besso <elie@meachei.com>
2016-06-06 22:28:17 -0300

    Update calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,14 +1,115 @@
-Kernel.puts ("Type 2 numbers")
-first = Kernel.gets().chomp()
-second = Kernel.gets().chomp()
-Kernel.puts ("What's the operation? add, subtract, multiply, divide")
-operation = Kernel.gets().chomp()
-if operation == "add"
-  Kernel.puts first.to_i() + second.to_i()
-elsif operation == "subtract"
-  Kernel.puts first.to_i() - second.to_i()
-elsif operation == "multiply"
-  Kernel.puts first.to_i() * second.to_i()
-else operation == "divide"
-  Kernel.puts first.to_f() / second.to_f()
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
+
+def integer?(input)
+  input.to_i.to_s == input
+end
+
+def float?(input)
+  input.to_f.to_s == input
+end
+
+def number?(input)
+  integer?(input) || float?(input)
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+name = ''
+loop do
+  prompt(MESSAGES['welcome'])
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt(MESSAGES['valid_name'])
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}!")
+
+loop do
+  first = ''
+
+  loop do
+    prompt(MESSAGES['first_number'])
+    first = Kernel.gets().chomp()
+
+    if number?(first)
+      break
+    else
+      prompt(MESSAGES['not_valid_number'])
+    end
+  end
+
+  second = ''
+
+  loop do
+    prompt(MESSAGES['second_number'])
+    second = Kernel.gets().chomp()
+
+    if number?(second)
+      break
+    else
+      prompt(MESSAGES['not_valid_number'])
+    end
+  end
+
+  operation_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operation_prompt)
+
+  operation = ''
+  loop do
+    operation = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operation)
+      break
+    else
+      prompt(MESSAGES['must_choose'])
+    end
+  end
+
+  prompt("#{operation_to_message(operation)} the two numbers...")
+
+  result = case operation
+           when '1'
+             first.to_i() + second.to_i()
+           when '2'
+             first.to_i() - second.to_i()
+           when '3'
+             first.to_i() * second.to_i()
+           when '4'
+             first.to_f() / second.to_f()
+           end
+
+  prompt("The result is #{result}")
+
+  prompt(MESSAGES['another_calculation'])
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt(MESSAGES['bye'])
